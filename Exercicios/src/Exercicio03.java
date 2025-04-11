e1a44b54555eb252d723be8a3140faadc2393300
GABRIEL WOLF LEBER <gabriel.leber@alunos.sc.senac.br>
2025-04-11 20:41:12 -0300

    Exercicio 03

---
+++ Exercicios/src/Exercicio03.java
@@ -0,0 +1,16 @@
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        int numero = 5;
+        int multiplo = 0;
+
+        for (int i = 0; i <= 10; i++) {
+            multiplo = i * numero;
+            System.out.println("A Tabuada do numero " + numero + " é : " + numero + " X " + i + " = " + multiplo);
+        }
+
+
+
+
+    }
+}
