73c0765b45e56fef85c90bf07bac9b5225567d8c
GABRIEL WOLF LEBER <gabriel.leber@alunos.sc.senac.br>
2025-04-11 20:32:53 -0300

    Exercicio 02

---
+++ Exercicios/src/Exercicio02.java
@@ -0,0 +1,14 @@
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        int N = 5;
+        int soma = 0;
+        for (int i = 1; i <= 5; i++) {
+            soma += i;
+        }
+        System.out.println("A soma é: " + soma);
+
+
+
+    }
+}
