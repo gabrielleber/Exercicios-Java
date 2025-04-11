afea4a63c4f19927a68f40f19a4ef6f1fc07598b
GABRIEL WOLF LEBER <gabriel.leber@alunos.sc.senac.br>
2025-04-11 20:54:16 -0300

    Exercicio 04

---
+++ Exercicios/src/Exercicio04.java
@@ -0,0 +1,13 @@
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        int N = 10;
+        for (int i = 0; i <= N; i++) {
+            if(i % 2 == 0) {
+                System.out.println("O numeros pares são : " + i);
+
+            }
+        }
+
+    }
+}
