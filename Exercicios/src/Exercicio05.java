8808828ffe375cb71fa5889c300e4f8d8bc664dc
GABRIEL WOLF LEBER <gabriel.leber@alunos.sc.senac.br>
2025-04-11 21:04:12 -0300

    Exercicio 5

---
+++ Exercicios/src/Exercicio05.java
@@ -0,0 +1,14 @@
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        int N = 10;
+        int i = 0;
+
+        while (i <= 10) {
+            if (i % 2 != 0) {
+                System.out.println("Os números ímpares são : " + i);
+            }
+            i++;
+        }
+    }
+}
